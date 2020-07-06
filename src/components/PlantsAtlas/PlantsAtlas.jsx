ae944a9ef0fc4fa941feb62bd999c0e3a1fe12ea
stosikmartyna <stosik.martyna@gmail.com>
2020-07-06 13:11:36 +0200

    Create function to cut description text.

--- src/components/PlantsAtlas/PlantsAtlas.jsx
+++ src/components/PlantsAtlas/PlantsAtlas.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Row, Col, Box } from '../_library/Containers';
 import { Break } from '../_library/Break';
 import { Button } from '../_library/Buttons';
 
 export const PlantsAtlas = () => {
-    const arrMock = [1,2,3]
-    
+    const arrMock = [1,2,3];
+    const plantDescriptionMock = 'Odmiana Fascinator Tricolor jest jedną z ciekawszych odmian barwnych maranty. Ta roślina doniczkowa tworzy gęsto ulistnione kępy sięgające 30 cm wysokości. Jej piękne, jajowate liście mają aksamitną fakturę i przykuwającą wzrok kolorystykę. Układają się regularnie w zwartą kępę z krótkimi pędami. Spód liści jest purpurowy, zewnętrzna strona natomiast wielobarwna. Ciemna zieleń powierzchni liści przeplata się z jasną, a wzdłuż głównych nerwów biegną jaskrawo pomarańczowe, kontrastujące wzory o łukowym rysunku. Środek każdego liścia zdobiony jest dodatkowo jasnymi akcentami. Wśród liści pojawiają się drobne, białe kwiaty praktycznie przez cały rok, jednak główną ozdobę stanowią barwne liście.'    
+
+    const cutText = (text) => text.substring(0, 125) + '...';
+
     return (
         <Row justify={'space-evenly'} wrap>
             {arrMock.map(div => {
@@ -14,11 +17,10 @@ export const PlantsAtlas = () => {
                         <Box maxWidth={22}>
                             <img src="plantsAtlasImg/maranta_fascinator_tricolor.jpg" alt="maranta_fascinator_tricolor" width='100%' />
                             <h2>Maranta Fascinator Tricolor</h2>
-                            <p> 
-                                Odmiana Fascinator Tricolor jest jedną z ciekawszych odmian barwnych maranty. Ta roślina doniczkowa tworzy gęsto ulistnione kępy sięgające 30 cm wysokości. Jej piękne, jajowate liście mają aksamitną fakturę i przykuwającą wzrok kolorystykę. Układają się regularnie w zwartą kępę z krótkimi pędami. Spód liści jest purpurowy, zewnętrzna strona natomiast wielobarwna. Ciemna zieleń powierzchni liści przeplata się z jasną, a wzdłuż głównych nerwów biegną jaskrawo pomarańczowe, kontrastujące wzory o łukowym rysunku. Środek każdego liścia zdobiony jest dodatkowo jasnymi akcentami. Wśród liści pojawiają się drobne, białe kwiaty praktycznie przez cały rok, jednak główną ozdobę stanowią barwne liście.
-
-                            </p>
-                            <Button text={'>>'} />
+                            <p>
+                                {cutText(plantDescriptionMock)}
+                             </p>
+                            <Button text={'>>'}  />
                         </Box>
                         <Break lines={1.5}/>
                     </Col>
